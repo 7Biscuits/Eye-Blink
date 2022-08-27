b8b589f3418932438fe126f6dfc4a91e1285516a
BiscuitButB <RudranshSrivastava2525@gmail.com>
2022-08-27 16:03:16 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+import time
+
+def findEyes(img, eyeCascade):
+    grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    eyes = eyeCascade.detectMultiScale(grayImg)
+    if len(eyes) == 0:
+        return 'EyesNotFound'
+    for (ex, ey, ew, eh) in eyes:
+        cv.rectangle(img, (ex, ey), (ex+ew, ey+eh), ( 0, 255, 0), 2)
+
+def main():
+    eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+    cam = cv.VideoCapture(0)
+
+    while True:
+        _ret, img = cam.read()
+
+        findEyes(img, eye_cascade)
+        cv.imshow('facedetect', img)
+
+        if cv.waitKey(5) == 27:
+            break
+        
+        if (findEyes(img, eye_cascade) == 'EyesNotFound'):
+            print('Eyes not found')
+            break
+
+if __name__ == '__main__':
+    main()
+    cv.destroyAllWindows()
