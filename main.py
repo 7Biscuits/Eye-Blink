aa962451c2619090268301c9f3f46bc89f9368d5
BiscuitButB <RudranshSrivastava2525@gmail.com>
2022-09-13 02:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2 as cv
+from cvzone.FaceDetectionModule import FaceDetector
 import time
 import serial
 
 cap = cv.VideoCapture(0)
+detector = FaceDetector()
 eyeCascade = eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-arduino = serial.Serial(port='COM11', baudrate=115200, timeout=.1)
+serialcomm = serial.Serial(port='COM4', baudrate='115200', timeout=.1)
 
 def findEyes(img, EyeCascade):
     grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -14,16 +16,16 @@ def findEyes(img, EyeCascade):
     for (ex, ey, ew, eh) in eyes:
         cv.rectangle(img, (ex, ey), (ex+ew, ey+eh), ( 0, 255, 0), 2)
 
-def returnArduinoData():
-    arduino.write(bytes('0', 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.read()
-    return data
-
 while True:
     success, img = cap.read()
+    img, bBoxes = detector.findFaces(img)
     if findEyes(img, eyeCascade) == 0:
-        returnArduinoData()
+        i = "onn"
+        serialcomm.write(i.encode())
+        
+    if findEyes(img, eyeCascade) != 0:
+        i = "off"
+        serialcomm.write(i.encode())
 
     cv.imshow('Face Detector', img)
     cv.waitKey(1)
